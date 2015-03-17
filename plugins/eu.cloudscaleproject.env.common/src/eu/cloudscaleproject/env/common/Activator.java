b4fcc61e7093a9e13f6a61d50d300f47253b45ed
Jure Polutnik <jure.polutnik@gmail.com>
2015-03-17 13:53:51 +0100

    Eclipse bug workaround - https://wiki.eclipse.org/Eclipse4/RCP/FAQ#Why_isn.27t_my_.40Inject-able.2F.40PostConstruct_methods_being_injected.3F

--- plugins/eu.cloudscaleproject.env.common/src/eu/cloudscaleproject/env/common/Activator.java
+++ plugins/eu.cloudscaleproject.env.common/src/eu/cloudscaleproject/env/common/Activator.java
@@ -11,43 +11,48 @@ import org.osgi.framework.BundleContext;
 import eu.cloudscaleproject.env.common.explorer.notification.ExplorerChangeNotifier;
 import eu.cloudscaleproject.env.common.notification.ResourceChangeReporter;
 
-public class Activator extends AbstractUIPlugin {
-	
+public class Activator extends AbstractUIPlugin
+{
+
 	IResourceChangeListener listener = null;
 
-	
 	// The plug-in ID
 	public static final String PLUGIN_ID = "eu.cloudscaleproject.env.common"; //$NON-NLS-1$
 
 	// The shared instance
 	public static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
-	public Activator() {
+	public Activator()
+	{
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);		
+	public void start(BundleContext context) throws Exception
+	{
+		super.start(context);
 		plugin = this;
-		
-		ResourcesPlugin.getWorkspace().addResourceChangeListener(
-				ExplorerChangeNotifier.getInstance(), IResourceChangeEvent.POST_BUILD);
-		
-		if(listener == null){
+
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(ExplorerChangeNotifier.getInstance(), IResourceChangeEvent.POST_BUILD);
+
+		if (listener == null)
+		{
 			try
 			{
-			listener = CloudscaleContext.createInstance(ResourceChangeReporter.class);
-			ResourcesPlugin.getWorkspace().addResourceChangeListener(listener, IResourceChangeEvent.POST_BUILD);
-			}
-			catch (Exception e)
+				//listener = CloudscaleContext.createInstance(ResourceChangeReporter.class);
+				//ResourcesPlugin.getWorkspace().addResourceChangeListener(listener, IResourceChangeEvent.POST_BUILD);
+			} catch (Exception e)
 			{
-				Logger.getLogger(getClass().getName()).severe("Unable to use CloudScale Context functionality. Product will not work as predicted.");
+				Logger.getLogger(getClass().getName()).severe(
+						"Unable to use CloudScale Context functionality. Product will not work as predicted.");
 				e.printStackTrace();
 			}
 		}
@@ -55,15 +60,20 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
-	public void stop(BundleContext context) throws Exception {
+	public void stop(BundleContext context) throws Exception
+	{
 		plugin = null;
 		super.stop(context);
-		
+
 		ResourcesPlugin.getWorkspace().removeResourceChangeListener(ExplorerChangeNotifier.getInstance());
-		
-		if(listener != null){
+
+		if (listener != null)
+		{
 			ResourcesPlugin.getWorkspace().removeResourceChangeListener(listener);
 			listener = null;
 		}
@@ -74,7 +84,8 @@ public class Activator extends AbstractUIPlugin {
 	 *
 	 * @return the shared instance
 	 */
-	public static Activator getDefault() {
+	public static Activator getDefault()
+	{
 		return plugin;
 	}
 
