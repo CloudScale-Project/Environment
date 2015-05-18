89a93fa4bd48f53f9fe1265726c282790453d127
Jure Polutnik <jure.polutnik@gmail.com>
2015-05-18 15:26:44 +0200

    updated crawler scirpt

--- misc/crawler/palladio-crawler.sh
+++ misc/crawler/palladio-crawler.sh
@@ -13,3 +13,9 @@ wget -q -O - "$@" $URL |
 sed "s/\(.*\)/\1\/features/g" update-sites-nightly.txt | xargs wget -q -O - "$@" | grep ".jar" | sed 's/.*href=\"\(.*.jar\)\".*/\1/g' | sed s/_.*.jar//g | sort | uniq > features.txt
 sed "s/\(.*\)/\1\/plugins/g" update-sites-nightly.txt | xargs wget -q -O - "$@" | grep ".jar" | sed 's/.*href=\"\(.*.jar\)\".*/\1/g' | sed s/_.*.jar//g | sort | uniq > plugins.txt
 
+cat update-sites-nightly.txt | sed -e 's/\(.*\)/<repository><id>???<\/id><layout>p2<\/layout><url>\1<\/url><\/repository>/g' >> update-sites-nightly-repository.txt
+
+cat features.txt | grep org.palladiosimulator.*.feature | grep -v .source. | sed -e 's/\(.*\)/<includes id="\1" version="0.0.0"\/>/g' >> palladio-includes.txt
+cat features.txt | grep org.palladiosimulator.*.feature | grep    .source. | sed -e 's/\(.*\)/<includes id="\1" version="0.0.0"\/>/g' >> palladio-includes.txt
+cat features.txt | grep de.uka.ipd.sdq.*.feature | grep    .source. | sed -e 's/\(.*\)/<includes id="\1" version="0.0.0"\/>/g' >> palladio-includes.txt
+cat features.txt | grep de.uka.ipd.sdq.*.feature | grep    .source. | sed -e 's/\(.*\)/<includes id="\1" version="0.0.0"\/>/g' >> palladio-includes.txt
