0251cca74b49b9cb34bd120777be52946d502fc7
Jure Polutnik <jure.polutnik@xlab.si>
2015-08-03 09:48:43 +0200

    Widgets - error/result/validation 'links'

--- plugins/eu.cloudscaleproject.env.toolchain/src/eu/cloudscaleproject/env/toolchain/ui/widgets/ResultWiget.java
+++ plugins/eu.cloudscaleproject.env.toolchain/src/eu/cloudscaleproject/env/toolchain/ui/widgets/ResultWiget.java
@@ -4,6 +4,7 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.graphics.Cursor;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -11,6 +12,7 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.statushandlers.StatusManager;
 
 import eu.cloudscaleproject.env.common.CommonResources;
+import eu.cloudscaleproject.env.common.ui.resources.SWTResourceManager;
 import eu.cloudscaleproject.env.toolchain.resources.types.EditorInputResource;
 import eu.cloudscaleproject.env.toolchain.resources.types.IConfigAlternative;
 import eu.cloudscaleproject.env.toolchain.util.OpenAlternativeUtil;
@@ -18,17 +20,14 @@ import eu.cloudscaleproject.env.toolchain.util.OpenAlternativeUtil;
 public class ResultWiget extends Composite
 {
 
-
 	private MouseAdapter mouseListener = new MouseAdapter()
 	{
-		@Override
-		public void mouseDoubleClick(MouseEvent e)
+		public void mouseUp(MouseEvent e)
 		{
 			if (!status.isOK())
 			{
 				StatusManager.getManager().handle(status, StatusManager.SHOW);
-			}
-			else
+			} else
 			{
 				OpenAlternativeUtil.openAlternative((EditorInputResource) alternative.getLastResult());
 			}
@@ -42,42 +41,53 @@ public class ResultWiget extends Composite
 
 	private IConfigAlternative alternative;
 
+	private Label lblMore;
+
 	public ResultWiget(Composite parent, int style, IConfigAlternative alternative)
 	{
 		super(parent, style);
 
 		this.alternative = alternative;
-		setLayout(new GridLayout(2, false));
+		setLayout(new GridLayout(3, false));
 
 		lblIcon = new Label(this, SWT.NONE);
 		lblIcon.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, true, 1, 1));
 
 		lblText = new Label(this, SWT.NONE);
-		lblText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, true, 1, 1));
+		lblText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, true, 1, 1));
 
-		lblText.addMouseListener(mouseListener);
+		lblMore = new Label(this, SWT.NONE);
+		lblMore.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, true, 1, 1));
+		lblMore.setForeground(SWTResourceManager.getColor(SWT.COLOR_LINK_FOREGROUND));
+		lblMore.setText("(more...)");
+
 		lblIcon.addMouseListener(mouseListener);
+		lblMore.addMouseListener(mouseListener);
+		lblMore.setCursor(new Cursor(getDisplay(), SWT.CURSOR_HAND));
 	}
-	
-	public void setStatus (IStatus status)
+
+	public void setStatus(IStatus status)
 	{
 		this.status = status;
 		updateStatus();
-		
+
 	}
 
 	private void updateStatus()
 	{
 		if (status != null && status.isOK())
 		{
-			lblText.setText("Success... See result.");
+			lblText.setText("Success. ");
+			lblMore.setText("(result...)");
 			lblIcon.setImage(CommonResources.THUMB_UP);
+
 		} else
 		{
-			lblText.setText("Problem occured... See error.");
+			lblText.setText("Problem occured.");
+			lblMore.setText("(error...)");
 			lblIcon.setImage(CommonResources.ERROR);
 		}
-		
+
 		layout();
 	}
 
