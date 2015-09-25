23035d8f5eba2acb4681945b93b54433f44d0bbc
Jure Polutnik <jure.polutnik@xlab.si>
2015-09-25 17:42:15 +0200

    Migration fixes - open perspectives, bottom trimbar addon

--- plugins/eu.cloudscaleproject.env.product/src/eu/cloudscaleproject/env/product/addons/BottomTrimbarAddon.java
+++ plugins/eu.cloudscaleproject.env.product/src/eu/cloudscaleproject/env/product/addons/BottomTrimbarAddon.java
@@ -1,4 +1,4 @@
- 
+
 package eu.cloudscaleproject.env.product.addons;
 
 import javax.inject.Inject;
@@ -10,24 +10,37 @@ import org.eclipse.e4.ui.model.application.ui.basic.MTrimBar;
 import org.eclipse.e4.ui.model.application.ui.basic.MTrimElement;
 import org.eclipse.e4.ui.model.application.ui.basic.MTrimmedWindow;
 import org.eclipse.e4.ui.workbench.UIEvents;
+import org.eclipse.swt.widgets.Display;
 import org.osgi.service.event.Event;
 
-public class BottomTrimbarAddon {
+public class BottomTrimbarAddon
+{
 
 	@Inject
 	@Optional
-	public void applicationStarted(@EventTopic(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE) Event event, MApplication app) {
-		
-		MTrimmedWindow window = (MTrimmedWindow) app.getChildren().get(0);
-		
-		for(MTrimBar trimbar : window.getTrimBars()){
-			for(MTrimElement element : trimbar.getChildren()){
-				if("org.eclipse.ui.ProgressBar".equals(element.getElementId())){
-					element.setToBeRendered(true);
-					element.setVisible(true);
+	public void applicationStarted(@EventTopic(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE) Event event, MApplication app)
+	{
+		final MTrimmedWindow window = (MTrimmedWindow) app.getChildren().get(0);
+
+		Display.getDefault().asyncExec(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				for (MTrimBar trimbar : window.getTrimBars())
+				{
+					for (MTrimElement element : trimbar.getChildren())
+					{
+						if ("org.eclipse.ui.ProgressBar".equals(element.getElementId()))
+						{
+							element.setToBeRendered(true);
+							element.setVisible(true);
+						}
+					}
 				}
+
 			}
-		}		
+		});
 	}
 
 }
--- plugins/eu.cloudscaleproject.env.product/src/eu/cloudscaleproject/env/product/handlers/OpenPerspectiveHandler.java
+++ plugins/eu.cloudscaleproject.env.product/src/eu/cloudscaleproject/env/product/handlers/OpenPerspectiveHandler.java
@@ -1,6 +1,7 @@
- 
+
 package eu.cloudscaleproject.env.product.handlers;
 
+import java.util.HashMap;
 import java.util.List;
 
 import javax.inject.Named;
@@ -16,56 +17,72 @@ import org.eclipse.swt.widgets.Display;
 
 import eu.cloudscaleproject.env.product.branding.CloudScaleBranding;
 
-public class OpenPerspectiveHandler {
-		
+public class OpenPerspectiveHandler
+{
+
+	// Second time CSE perspective is not found anymore
+	private static HashMap<String, MPerspective> _WORKAROUND_MAP = new HashMap<String, MPerspective>();
+
 	@Execute
-	public void execute(@Named("PerspectiveId") String perspectiveId, 
-						EModelService service, 
-						final MApplication application, final EPartService partService) {
-		
-	    final MPerspective element = (MPerspective) service.find(perspectiveId, application);
-	    
+	public void execute(@Named("PerspectiveId") String perspectiveId, EModelService service, final MApplication application,
+			final EPartService partService)
+	{
+
+		MPerspective element = (MPerspective) service.find(perspectiveId, application);
+
+		if (element == null)
+			element = _WORKAROUND_MAP.get(perspectiveId);
+		else
+			_WORKAROUND_MAP.put(perspectiveId, element);
+
 		// Switch perspective
-		if (element != null) {
-			
-			BusyIndicator.showWhile(Display.getDefault(), new Runnable() {
-				
+		if (element != null)
+		{
+
+			final MPerspective fElement = element;
+			BusyIndicator.showWhile(Display.getDefault(), new Runnable()
+			{
 				@Override
-				public void run() {
-					switchPerspective(application, partService, element);
+				public void run()
+				{
+					switchPerspective(application, partService, fElement);
 				}
 			});
-	
-		}
-		else{
+		} else
+		{
 			throw new IllegalArgumentException("Perspective with perspectiveId: " + perspectiveId + " does not exist!");
 		}
-		
+
 	}
-	
-	public void switchPerspective(MApplication application, final EPartService partService, final MPerspective element){
-		
-		try {
+
+	public void switchPerspective(MApplication application, final EPartService partService, final MPerspective element)
+	{
+
+		try
+		{
 			partService.switchPerspective(element);
-		} 
-		catch (Exception e) {
-			
-			//workaround: Application does not have an active window Exception....
+		} catch (Exception e)
+		{
+
+			// workaround: Application does not have an active window
+			// Exception....
 			List<MWindow> windows = application.getChildren();
 			final MWindow mWindow = windows.get(0);
-			
-			Display.getDefault().syncExec(new Runnable() {
+
+			Display.getDefault().syncExec(new Runnable()
+			{
 				@Override
-				public void run() {
-					//get the main window and make it active
-					//somehow this solves the problem
+				public void run()
+				{
+					// get the main window and make it active
+					// somehow this solves the problem
 					mWindow.getContext().activate();
 					partService.switchPerspective(element);
 				}
 			});
 		}
-		
+
 		CloudScaleBranding.initProjectExplorer();
-		
+
 	}
 }
