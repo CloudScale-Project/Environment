102d3e8a5bc34f5846b642718dc3949c2c75b52e
Jure Polutnik <jure.polutnik@xlab.si>
2015-10-13 15:51:16 +0200

    Workaround - manually find MPerspective

--- plugins/eu.cloudscaleproject.env.product/src/eu/cloudscaleproject/env/product/handlers/OpenPerspectiveHandler.java
+++ plugins/eu.cloudscaleproject.env.product/src/eu/cloudscaleproject/env/product/handlers/OpenPerspectiveHandler.java
@@ -1,8 +1,8 @@
- 
+
 package eu.cloudscaleproject.env.product.handlers;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.logging.Logger;
 
 import javax.inject.Named;
 
@@ -17,58 +17,75 @@ import org.eclipse.swt.widgets.Display;
 
 import eu.cloudscaleproject.env.product.branding.CloudScaleBranding;
 
-public class OpenPerspectiveHandler {
-	
-	private final Logger logger = Logger.getLogger(OpenPerspectiveHandler.class.getName());
-		
+public class OpenPerspectiveHandler
+{
+
 	@Execute
-	public void execute(@Named("PerspectiveId") String perspectiveId, 
-						EModelService service, 
-						final MApplication application, final EPartService partService) {
-		
-	    final MPerspective element = (MPerspective) service.find(perspectiveId, application);
-	    	    
+	public void execute(@Named("PerspectiveId") String perspectiveId, EModelService service, final MApplication application,
+			final EPartService partService)
+	{
+
+		// Finding main perspective by ID fails for Main perspectives. Eclipse add '.<main_perspective>' to the ID
+		// WORKAROUND: Get all perspectives and find element which ID starts with passed perspectiveId
+		MPerspective element = null;
+		List<MPerspective> elements = service.findElements(application, null, MPerspective.class, null);
+		for (MPerspective p : elements)
+		{
+			if (p.getElementId().startsWith(perspectiveId))
+			{
+				element = p;
+				break;
+			}
+		}
+
 		// Switch perspective
-		if (element != null) {
-			
-			BusyIndicator.showWhile(Display.getDefault(), new Runnable() {
-				
+		if (element != null)
+		{
+
+			final MPerspective fElement = element;
+			BusyIndicator.showWhile(Display.getDefault(), new Runnable()
+			{
 				@Override
-				public void run() {
-					switchPerspective(application, partService, element);
+				public void run()
+				{
+					switchPerspective(application, partService, fElement);
 				}
 			});
-	
+		} else
+		{
+			throw new IllegalArgumentException("Perspective with perspectiveId: " + perspectiveId + " does not exist!");
 		}
-		else{
-			logger.warning("Perspective with perspectiveId: " + perspectiveId + " does not exist!");
-		}
-		
+
 	}
-	
-	public void switchPerspective(MApplication application, final EPartService partService, final MPerspective element){
-		
-		try {
+
+	public void switchPerspective(MApplication application, final EPartService partService, final MPerspective element)
+	{
+
+		try
+		{
 			partService.switchPerspective(element);
-		} 
-		catch (Exception e) {
-			
-			//workaround: Application does not have an active window Exception....
+		} catch (Exception e)
+		{
+
+			// workaround: Application does not have an active window
+			// Exception....
 			List<MWindow> windows = application.getChildren();
 			final MWindow mWindow = windows.get(0);
-			
-			Display.getDefault().syncExec(new Runnable() {
+
+			Display.getDefault().syncExec(new Runnable()
+			{
 				@Override
-				public void run() {
-					//get the main window and make it active
-					//somehow this solves the problem
+				public void run()
+				{
+					// get the main window and make it active
+					// somehow this solves the problem
 					mWindow.getContext().activate();
 					partService.switchPerspective(element);
 				}
 			});
 		}
-		
+
 		CloudScaleBranding.initProjectExplorer();
-		
+
 	}
 }
