2b2cc95a8e328f09d872e7c0e5cf45db9763220a
Vito Čuček <vito.cucek@xlab.si>
2015-10-20 17:45:32 +0200

    ExplorerService selection hack removed

--- plugins/eu.cloudscaleproject.env.toolchain.explorer/src/eu/cloudscaleproject/env/toolchain/explorer/addons/ExplorerService.java
+++ plugins/eu.cloudscaleproject.env.toolchain.explorer/src/eu/cloudscaleproject/env/toolchain/explorer/addons/ExplorerService.java
@@ -1,8 +1,5 @@
 package eu.cloudscaleproject.env.toolchain.explorer.addons;
 
-import org.eclipse.swt.widgets.Display;
-
-import eu.cloudscaleproject.env.common.BatchExecutor;
 import eu.cloudscaleproject.env.toolchain.explorer.Explorer;
 import eu.cloudscaleproject.env.toolchain.explorer.IExplorerNode;
 import eu.cloudscaleproject.env.toolchain.services.IExplorerService;
@@ -11,28 +8,8 @@ public class ExplorerService implements IExplorerService{
 
 	@Override
 	public void setSelection(final Object data) {
-		Explorer.getInstance().refreshAll();
-
-		BatchExecutor.getInstance().addUITask(this, data, new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				
-				Display.getDefault().asyncExec(new Runnable()
-				{
-					
-					@Override
-					public void run()
-					{
-				IExplorerNode node = Explorer.getInstance().findNode(data);
-						// TODO Auto-generated method stub
-				Explorer.getInstance().setSelection(node);
-						
-					}
-				});
-			}
-		});
+		IExplorerNode node = Explorer.getInstance().findNode(data);
+		Explorer.getInstance().setSelection(node);		
 	}
 
 
