2baf9957284fd322ac87a4c4929e576ceecca2bf
Jure Polutnik <jure.polutnik@xlab.si>
2015-11-05 14:09:36 +0100

    IntroScreen - Cursor on buttons

--- plugins/eu.cloudscaleproject.env.product/src/eu/cloudscaleproject/env/product/gui/ButtonImg.java
+++ plugins/eu.cloudscaleproject.env.product/src/eu/cloudscaleproject/env/product/gui/ButtonImg.java
@@ -1,36 +1,43 @@
 package eu.cloudscaleproject.env.product.gui;
 
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Cursor;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
 
-public class ButtonImg {
+public class ButtonImg extends Composite{
 	
+	static Cursor handCursor = Display.getDefault().getSystemCursor(SWT.CURSOR_HAND);
 	public Label img;
 	
 	public ButtonImg(Composite parent, Image image, int width, int height) {
 		
-		Composite c_back = new Composite(parent, SWT.NONE);
+		super(parent, SWT.NONE);
+
 		FillLayout layout = new FillLayout();
 		GridData gd_back = new GridData(GridData.HORIZONTAL_ALIGN_CENTER, GridData.VERTICAL_ALIGN_CENTER,
 				false, false);
 		gd_back.widthHint = width;
 		gd_back.heightHint = height;
-		c_back.setLayoutData(gd_back);
-		c_back.setLayout(layout);
-		c_back.setVisible(true);
+		this.setLayoutData(gd_back);
+		this.setLayout(layout);
+		this.setVisible(true);
 		
-		img = new Label(c_back, SWT.NONE);
+		img = new Label(this, SWT.NONE);
 		img.setImage(image);
 		img.setBounds(image.getBounds());
+		
+		this.setCursor(handCursor);
 	}
 	
 	public void addSelectionListener(final SelectionListener listener){
