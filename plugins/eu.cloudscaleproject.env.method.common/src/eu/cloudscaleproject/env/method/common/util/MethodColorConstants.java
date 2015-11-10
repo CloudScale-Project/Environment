e1eb9115943c343cea60253ae0d23bcc339e330f
Jure Polutnik <jure.polutnik@xlab.si>
2015-11-10 14:06:41 +0100

    Icon updates - cubic interpolation

--- plugins/eu.cloudscaleproject.env.method.common/src/eu/cloudscaleproject/env/method/common/util/MethodColorConstants.java
+++ plugins/eu.cloudscaleproject.env.method.common/src/eu/cloudscaleproject/env/method/common/util/MethodColorConstants.java
@@ -4,8 +4,8 @@ import org.eclipse.graphiti.util.IColorConstant;
 public interface MethodColorConstants extends IColorConstant
 {
 	
-	String STATUS_NODE = "a1c9d5"; // ~blue
-	String STATUS_NODE_DISABLED = "cdcdcd"; // ~gray
+	String STATUS_NODE = "f1c9d5"; // ~blue
+	String STATUS_NODE_DISABLED = STATUS_NODE;//"cdcdcd"; // ~gray
 	String STATUS_NODE_WARNING = "ffd073";  // ~orange
 	String STATUS_NODE_ERROR = "ffaa92";  // ~orange
 	String STATUS_NODE_DONE = "91e5a5";		// ~green
@@ -15,9 +15,9 @@ public interface MethodColorConstants extends IColorConstant
 	String REQUIREMENT_ERROR = STATUS_NODE_ERROR;
 	String REQUIREMENT_DONE = STATUS_NODE_DONE;
 
-	String ACTION = STATUS_NODE;
-	String COMMAND = STATUS_NODE;
-	String CONTAINER = STATUS_NODE;
+	String ACTION = "567092";//STATUS_NODE;
+	String COMMAND = "567092";//STATUS_NODE;
+	String CONTAINER = "567092";//STATUS_NODE;
 
 
 }
