6062711c729c1af974480eb65e0ef014d1c950a4
Vito Čuček <vito.cucek@xlab.si>
2015-11-10 15:21:31 +0100

    NodeDynamic shows 'create new node' if empty

---
+++ plugins/eu.cloudscaleproject.env.analyser/src/eu/cloudscaleproject/env/analyser/handlers/CreateModelHandler.java
@@ -0,0 +1,34 @@
+ 
+package eu.cloudscaleproject.env.analyser.handlers;
+
+import javax.inject.Named;
+
+import org.eclipse.e4.core.contexts.Active;
+import org.eclipse.e4.core.di.annotations.CanExecute;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.annotations.Optional;
+
+import eu.cloudscaleproject.env.analyser.ModelUtils;
+import eu.cloudscaleproject.env.toolchain.ModelType;
+import eu.cloudscaleproject.env.toolchain.resources.types.EditorInputEMF;
+import eu.cloudscaleproject.env.toolchain.resources.types.IEditorInputResource;
+
+public class CreateModelHandler {
+	
+	@Execute
+	public void execute(@Active IEditorInputResource eir, 
+						@Named("eu.cloudscaleproject.env.analyser.commandparameter.fileextension") String extension) {
+
+		if(eir instanceof EditorInputEMF){
+			ModelUtils.executeCreateModels((EditorInputEMF)eir, new ModelType[]{ModelType.getModelType(extension)});
+		}
+	}
+	
+	@CanExecute
+	public boolean canExecute(@Optional @Active IEditorInputResource eir){
+		if(eir instanceof EditorInputEMF){			
+			return true;
+		}
+		return false;
+	}
+}
