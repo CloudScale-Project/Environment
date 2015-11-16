d5d2035e10e21d205d2db479dacce89377ec6880
Jure Polutnik <jure.polutnik@xlab.si>
2015-11-16 13:19:09 +0100

    CS Explorer : Open in explorer action

--- plugins/eu.cloudscaleproject.env.toolchain.explorer/src/eu/cloudscaleproject/env/toolchain/explorer/addons/ExplorerServiceAddon.java
+++ plugins/eu.cloudscaleproject.env.toolchain.explorer/src/eu/cloudscaleproject/env/toolchain/explorer/addons/ExplorerServiceAddon.java
@@ -4,13 +4,17 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 
+import org.eclipse.e4.core.contexts.Active;
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.core.di.extensions.EventTopic;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.workbench.UIEvents;
 import org.osgi.service.event.Event;
 
+import eu.cloudscaleproject.env.toolchain.explorer.handlers.SelectInExplorerHandler;
+import eu.cloudscaleproject.env.toolchain.resources.types.IEditorInputResource;
 import eu.cloudscaleproject.env.toolchain.services.IExplorerService;
 
 public class ExplorerServiceAddon {
@@ -36,6 +40,16 @@ public class ExplorerServiceAddon {
 		//remove diagram service from the context
 		context.remove(IExplorerService.class);
 		context.remove(ExplorerService.class);
+	}
+	
+	@Inject @Optional
+	public void setActivePart(@Active MPart part){
+		
+		if (!part.getElementId().equals("eu.cloudscaleproject.env.toolchain.explorer.part.explorer"))
+		{
+			IEditorInputResource eir = part.getContext().get(IEditorInputResource.class);
+			if (eir != null) SelectInExplorerHandler.eir = eir;
+		}
 	}
 	
 }
---
+++ plugins/eu.cloudscaleproject.env.toolchain.explorer/src/eu/cloudscaleproject/env/toolchain/explorer/handlers/SelectInExplorerHandler.java
@@ -0,0 +1,23 @@
+ 
+package eu.cloudscaleproject.env.toolchain.explorer.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+import eu.cloudscaleproject.env.toolchain.explorer.Explorer;
+import eu.cloudscaleproject.env.toolchain.explorer.IExplorerNode;
+import eu.cloudscaleproject.env.toolchain.resources.types.IEditorInputResource;
+
+public class SelectInExplorerHandler {
+	
+	public static IEditorInputResource eir;
+
+	@Execute
+	public void execute() {
+		
+		if (eir == null) return;
+
+		IExplorerNode en = Explorer.getInstance().findNode(eir);
+
+		if (en != null) Explorer.getInstance().setSelection(en);
+	}
+}
