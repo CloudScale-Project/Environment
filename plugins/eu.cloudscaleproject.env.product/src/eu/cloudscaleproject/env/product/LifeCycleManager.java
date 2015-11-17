9b1f89b945f8ee454a6db3e30adf1cea572e546e
Vito Čuček <vito.cucek@xlab.si>
2015-11-17 12:05:16 +0100

    Window close handler

--- plugins/eu.cloudscaleproject.env.product/src/eu/cloudscaleproject/env/product/LifeCycleManager.java
+++ plugins/eu.cloudscaleproject.env.product/src/eu/cloudscaleproject/env/product/LifeCycleManager.java
@@ -1,7 +1,19 @@
 package eu.cloudscaleproject.env.product;
 
+import javax.inject.Inject;
+
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.di.UIEventTopic;
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
+import org.eclipse.e4.ui.workbench.UIEvents;
 import org.eclipse.e4.ui.workbench.lifecycle.PostContextCreate;
+import org.eclipse.e4.ui.workbench.lifecycle.PreSave;
+import org.eclipse.e4.ui.workbench.lifecycle.ProcessAdditions;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.e4.ui.workbench.modeling.IWindowCloseHandler;
 
 import eu.cloudscaleproject.env.common.CloudscaleContext;
 
@@ -12,4 +24,35 @@ public class LifeCycleManager {
 		CloudscaleContext.initialize(applicationContext);
 	}
 
+
+	@ProcessAdditions
+	void processAdditions(MApplication app, EModelService modelService) {
+		
+	}
+	
+	@Inject
+    @Optional
+    public void startupComplete(@UIEventTopic(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE) MApplication application,
+            																			 EModelService modelService){
+
+        MWindow window = (MWindow) modelService.find("eu.cloudscaleproject.env.product.trimmedwindow.0", application);
+        window.getContext().set(IWindowCloseHandler.class, new WindowCloseHandler());
+    }
+
+	@PreSave
+	public void preSave() {
+
+	}
+
+	private static class WindowCloseHandler implements IWindowCloseHandler {
+
+		@Override
+		public boolean close(MWindow window) {
+			
+			EPartService partService = window.getContext().get(EPartService.class);
+			partService.saveAll(true);
+
+			return true;
+		}
+	}
 }
