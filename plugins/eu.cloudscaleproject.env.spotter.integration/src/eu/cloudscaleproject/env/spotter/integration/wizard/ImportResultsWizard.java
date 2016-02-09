2256dcf1259e427d82f07c752a2b6220a9eae99d
Vito Čuček <vito.cucek@xlab.si>
2016-02-09 17:53:03 +0100

    Static -> Dynamic spotter integration plugin project.

---
+++ plugins/eu.cloudscaleproject.env.spotter.integration/src/eu/cloudscaleproject/env/spotter/integration/wizard/ImportResultsWizard.java
@@ -0,0 +1,13 @@
+package eu.cloudscaleproject.env.spotter.integration.wizard;
+
+import org.eclipse.jface.wizard.Wizard;
+
+public class ImportResultsWizard extends Wizard{
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
